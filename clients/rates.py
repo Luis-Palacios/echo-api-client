320a257fc868f40575b97bd6c08993570603595d
Luis Palacios <lrpalaciosdev@gmail.com>
2019-06-03 17:18:15 +0000

    add item rate class and destination type enum

--- clients/rates.py
+++ clients/rates.py
@@ -1,6 +1,24 @@
 """rates module
 """
+from enum import Enum
 import requests
+
+
+class RateItem(object):
+    nmfc_class = ''
+    weight = 0
+
+    def __init__(self, nmfc_class, weight):
+        super().__init__()
+        self.nmfc_class = nmfc_class
+        self.weight = weight
+
+
+class DestinationType(Enum):
+    RESIDENTIAL = 'RESIDENTIAL'
+    CONSTRUCTIONSITE = 'CONSTRUCTIONSITE'
+    TRADESHOW = 'TRADESHOW'
+    BUSINESS = 'BUSINESS'
 
 
 def estimate(access_token, origin_postal_code, destination_postal_code,
@@ -14,10 +32,19 @@ def estimate(access_token, origin_postal_code, destination_postal_code,
             'OriginCountryCode': 'US',
             'DestinationPostalCode': destination_postal_code,
             'DestinationCountryCode': 'US',
-            'DestinationType': destination_type,
+            'DestinationType': destination_type.value,
             'PickUpDate': pickup_date,
             'PalletQuantity': pallet_quantity,
             'UnitOfWeight': 'LB',
-            'items': items
+            'items': __map_items__(items)
         })
     return response.json()
+
+def __map_items__(items):
+    result = []
+    for item in items:
+        result.append({
+            'NmfcClass': item.nmfc_class,
+            'Weight': item.weight
+        })
+    return result
