ac3aadb8d05002f50f5a8a5c34678a33874f3dac
Luis Palacios <lrpalaciosdev@gmail.com>
2019-06-03 15:15:52 +0000

    change authorization header token type

--- clients/rates.py
+++ clients/rates.py
@@ -5,7 +5,7 @@ import requests
 
 def estimate(access_token, origin_postal_code, destination_postal_code,
              destination_type, pickup_date, pallet_quantity, items):
-    headers = {'Authorization': 'Bearer {}'.format(access_token)}
+    headers = {'Authorization': 'Basic {}'.format(access_token)}
     response = requests.post(
         'https://restapi.echo.com/v2/rates?Take&SCAC&TransitDays&Guaranteed&CarrierName',
         headers=headers,
